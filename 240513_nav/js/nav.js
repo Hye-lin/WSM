667b065644cc6b4600e8390a8db538392aa94141
박혜린 <w2315@e-mirim.hs.kr>
2024-06-11 15:54:09 +0900

    school-market 준비 완료

--- 240513_nav/js/nav.js
+++ 240513_nav/js/nav.js
@@ -1,36 +1,39 @@
-//반응형 웹의 자바스크립트에서 하는 일
-//1.html 요소를 가져와js 변수에 저장한다.
-//2.이벤트를 처리한다.(click, foucus, mouseup등)
-//3.클래스를 수정하여 스타일을 적용한다.
+/*  반응형 웹의 JS에서 하는 일
+1. HTML 요소를 가져와 JS 변수에 저장한다.
+2. 이벤트를 처리한다. (click, foucus, mouseup 등)
+3. 클래스를 수정하여 스타일을 적용한다.
 
+*/
+// .nav-list
+// .nav-toggle
 
+// 햄버거 메뉴 -> 닫기 버튼
+// .nav-list 아래로 내리자 
 
-//.nav-list
-//.nav-toggle
+// 닫기 버튼 -> 햄버거 메뉴
+// .nav-list 위로 올리자
 
-//햄버거 메뉴->닫기 버튼
-// .nav-list아레로 내리자
-
-//닫기 버튼->햄버거 메뉴
-// .nav-list위로 올리자
 
 const toggleMenu = () => {
-    //HTML요소->js
-    const navToggleDiv = document.getAnimationsByClassName("nav-toggle")[0];
-    const navListU1 = document.getElementsByClassName("nav-list")[0];
-    const toggleI = navToggleDiv.getAnimationsByClassName("i")[0];
+    // HTML 요소 -> js
+    const navToggleDiv = document.getElementsByClassName("nav-toggle")[0]; // .nav-toggle(햄버거메뉴,닫기) 가져오자 
+    const toggleI = navToggleDiv.getElementsByTagName("i")[0];             // .nav-toggle > <i> 가져오자
+    const navListUl = document.getElementsByClassName("nav-list")[0];      // .nav-list 가져오자
 
-    navToggleDiv.onclick = () => {
-        //class에 show-menu를 붙이거나/때자
-        navListU1.classList.toggle("show-menu");
+    navToggleDiv.onclick = () => { /*onclick 이벤트*/ 
+        // class에 show-menu를 붙이거나/때자
+        navListUl.classList.toggle("show-menu");
 
-        //toggle icon 바꾸자:di-list <-> di-x-lg
-        toggleI.classList.toggle("di-list");
+        // toggle icon 바꾸자 : bi-list <-> bi-x-lg
+        toggleI.classList.toggle("bi-lsit");  
+        toggleI.classList.toggle("bi-x-lg");
+
+        //방법 2)
         //toggleI.classList.add("bi-list");
-         //toggleI.classList.add("bx-lg");
-          //toggleI.classList.add("bi-lg");
-           //toggleI.classList.add("bi-list");
-        toggleI.classList.toggle("di-x-lg");
+        //toggleI.classList.remove("bi-x-lg")
+        //toggleI.classList.add("bi-x-list");
+        //toggleI.classList.remove("bi-list")
     }
+
 }
 toggleMenu();
