ba070756854d36bce6df38e9442a82785e95a6ac
박혜린 <w2315@e-mirim.hs.kr>
2024-09-10 15:57:20 +0900

    css grid 연습.  이전/다음달 버튼 클릭처리

--- 240827_grid연습/calendar.js
+++ 240827_grid연습/calendar.js
@@ -2,6 +2,21 @@
 
 // 현재 날짜 구하기
 let currentDate = new Date();
+
+//이전 달 버튼 이벤트 처리하기 
+const prevMonthButton = document.getElementById("prev-month");
+prevMonthButton.addEventListener("click",() => changeMonth (-1));
+
+// => 얘는 이름이 없는 함수이다.
+
+//다음 달 버튼 이벤트 처리하기
+    const nextMonthButton = document.querySelector("#next-month"); /*console.log('이전달)) 하면 안돼. console.log() 함수 실행한 결과를 클릭했을때 실행하는거야. 즉 아무일도 안함 */
+    nextMonthButton.onclick = () => changeMonth(1);
+
+const changeMonth = (delta) => {
+     currentDate.setMonth(currentDate.getMonth() + delta );
+    setCalendarHeader(currentDate);
+}
 const setCalendarHeader = (date) => {
     // 연도 구하기
     const year = date.getFullYear();
@@ -17,6 +32,9 @@ const setCalendarHeader = (date) => {
 setCalendarHeader(currentDate);
 
 // 일 구하기
+const setCalnedar = (data) => {
+    
+}
 
 // 첫 날의 요일 구하기
 // 마지막 날짜 구하기
