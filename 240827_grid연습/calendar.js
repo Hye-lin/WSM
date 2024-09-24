1ccc1af150d9cfa56b7bef015aaaf2877a1411f4
박혜린 <w2315@e-mirim.hs.kr>
2024-09-24 16:07:03 +0900

    css grid연습.calendar 요일,이번달 날짜 만들기

--- 240827_grid연습/calendar.js
+++ 240827_grid연습/calendar.js
@@ -1,45 +1,82 @@
-// 달력 만들기
+d// 달력
+const calendarContainerDiv = document.querySelector("#calendar-container");
 
-// 현재 날짜 구하기
+// 현재 날짜 구하자
 let currentDate = new Date();
 
-//이전 달 버튼 이벤트 처리하기 
-const prevMonthButton = document.getElementById("prev-month");
-prevMonthButton.addEventListener("click",() => changeMonth (-1));
-
-// => 얘는 이름이 없는 함수이다.
-
-//다음 달 버튼 이벤트 처리하기
-    const nextMonthButton = document.querySelector("#next-month"); /*console.log('이전달)) 하면 안돼. console.log() 함수 실행한 결과를 클릭했을때 실행하는거야. 즉 아무일도 안함 */
-    nextMonthButton.onclick = () => changeMonth(1);
-
-const changeMonth = (delta) => {
-     currentDate.setMonth(currentDate.getMonth() + delta );
-    setCalendarHeader(currentDate);
-}
 const setCalendarHeader = (date) => {
-    // 연도 구하기
+    // 연도 구하자
     const year = date.getFullYear();
-    // 달 구하기
-    const month = date.getMonth();
+    // 달 구하자
+    const month = date.getMonth();  // 0: 1월, 1: 2월
 
-    titleString = `${year}년 ${month + 1}월`
-    console.log(titleString)
-
+    titleString = `${year}년 ${month + 1}월`;
     const calendarHeaderH1 = document.querySelector("#calendar-header h1");
-    calendarHeaderH1.innerHTML = titleString
-};
-setCalendarHeader(currentDate);
-
-// 일 구하기
-const setCalnedar = (data) => {
-    
+    calendarHeaderH1.innerHTML = titleString;
+}
+const changeMonth = (delta) => {
+    currentDate.setMonth(currentDate.getMonth() + delta);
+    setCalendarHeader(currentDate);
+    setCalendar(currentDate);
 }
 
-// 첫 날의 요일 구하기
-// 마지막 날짜 구하기
-// 마지막 날의 요일 구하기
+// 이전 달 버튼 이벤트 처리하자
+const prevMonthButton = document.getElementById("prev-month");
+prevMonthButton.addEventListener("click", () => changeMonth(-1)); // console.log() 함수 실행한 결과를 클릭했을 때 실행하는거야. 즉 아무일도 안 함
 
-// 이전 달 뒷날짜 구하기
+// 다음 달 버튼 이벤트 처리하자
+const nextMonthButton = document.querySelector("#next-month");
+nextMonthButton.onclick = () => changeMonth(1);
 
-// 다음 달 앞날짜 구하기s
+// 일 구하자
+const setCalendar = (date) => {
+    const year = date.getFullYear();
+    const month = date.getMonth();
+    // 첫 날의 요일 구하자: 이전달 뒷 날짜 쓰기 위하여
+    const firstDay = new Date(year, month, 1).getDay(); //0: 일, 6: 토
+    console.log(firstDay);
+
+    // 마지막 날짜 구하자: 요일 구하기 위하여
+    const lastDate = new Date(year, month + 1, 0)
+    console.log(lastDate.getDate());
+
+    // 마지막 날의 요일 구하자: 다음달 앞 날짜 쓰기 위하여
+    const lastDay = lastDate.getDay();
+    console.log(lastDay);
+
+    // 일월화수목금토
+    let weekNameString = "";
+    const weekNames = "일월화수목금토";
+    const weekNamesArray = weekNames.split("");
+    weekNamesArray.forEach((weekName) => {
+        weekNameString += `<div class="item week-name">${weekName}</div>`
+    });
+
+    calendarContainerDiv.innerHTML = weekNameString;
+
+    // 이전 달 뒷날짜 구하자
+    //0~이번달 1일의 요일-1까지 이전달 마지막 날짜 - 이번달 1일의 요일 +1(시작날짜)부터 +1해서 쓰자
+    //0~1까지 마지막 날짜 -n개 부터 차례로 날짜 쓰자. n:마지막 날짜에서 -이번달 1일의 요일 +1 부터 +1해서 쓰자
+
+
+    // 이번달 날짜들 쓰자: 1~30: 1~lastDate.getDate()
+    // let dateString = "";
+    // for (let date = 1; date <= lastDate.getDate(); date++) {
+    //     dateString += `<div class="item">${date}</div>`;
+    // }
+    // calendarContainerDiv.innerHTML += dateString;
+    //div 요소 만들자, class에 item 넣자 text에 날짜 넣자. calendarContainerDiv 에 자식으로 붙이자
+    for (let date = 1; date <= lastDate.getDate; date++) {
+        let currentMonthDateDiv = document.createElement("div"); //<div></div>
+        currentMonthDateDiv.className = "item";   //<div class"item"></div>
+        currentMonthDateDiv.textContent = date;     //<div class = "item">1</div>"
+        calendarContainerDiv.appendChild(currentMonthDateDiv); //<div id="calendar-container"><div class = "item">1</div>"
+        
+    }
+    
+    // 다음 달 앞날짜 구하자
+    // 이번달 마지막 날의 요일+1~6까지 1부터 차례대로 날짜 쓰자
+}
+
+setCalendarHeader(currentDate);
+setCalendar(currentDate);
