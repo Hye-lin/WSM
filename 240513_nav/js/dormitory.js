ca561efd46efb5f5fb99e66c6094373be97ca75f
박혜린 <w2315@e-mirim.hs.kr>
2024-11-12 16:08:25 +0900

    dormitory 세탁기 예약 현황표

--- 240513_nav/js/dormitory.js
+++ 240513_nav/js/dormitory.js
@@ -1,7 +1,7 @@
 let allData;        // 초기 설정에 필요한 모든 데이터 : 세탁기, 시간, 호실
 let weeklyReservations;      // 미리 정해진 요일별 예약 데이터
-let newReservation;    // 사용자가 새롭게 지금 입력하는 예약 정보, 1페이지에서 초기화 하기
-let reservations;       // 사용자가 예약한 정보들의 덩어리
+let newReservation;         //사용자가 새롭게 지금 입력하는 예약정보. 1페이지에서 초기화 하자
+let reservations = [];           //사용자가 에약한 정보들의 덩어리
 
 // selection-item 요소 가져오기
 const selectionItemDivs = document.getElementsByClassName("selection-item");
@@ -15,6 +15,8 @@ const selectionRoomNameDiv = document.querySelector("#selection-room-name");
 const boardDiv = document.querySelector("#board");
 const roomSelect = document.getElementById("room");
 const nameInput = document.getElementById("name");
+const boardContainerDiv = document.getElementsByClassName("board-container")[0];
+let boardContainerDivInitString = boardContainerDiv.innerHTML;
 
 // 4개 한번에 다 모으기
 const pageDivs = [calendarDiv, selectionWashingmachineTimeDiv, selectionRoomNameDiv, boardDiv];
@@ -64,44 +66,47 @@ const setPage = (page) => {
     // show pageDiv 1
     pageDivs[page-1].style.display = "block";
 
-    if(page === 2) {    // 시간 선택 : 세탁기, 시간
+    if(page === 2) {        //시간 선택 : 세탁기, 시간
         initWashingmachineTime();
-
-    } else if (page === 3) {    // 호실 이름
-        //세탁기 번호,시간 보관하자
-        newReservation.washingmachine = washingmachineSelect.value; //세탁기 option에서 사용자가 선택한 세탁기의value속성값을 가져오자
+    }
+    else if(page === 3) {   //호실 이름
+        // 세탁기 번호, 시간 보관하기
+        newReservation.washingmachine = washingmachineSelect.value;   // 세탁기 option에서 사용자가 선택한 세탁기의 value 속성값을 가져오기
         newReservation.time = timeSelect.value;
-        initRoomName();
 
-    } else if (page === 4) {    // 세탁기 예약 현황표
-        //호실,이름 보관하자
+        initRoomName();
+    }
+    else if(page === 4) {   //세탁기 예약 현황표
+        // 호실, 이름 보관하기
         newReservation.room = roomSelect.value;
         newReservation.name = nameInput.value;
-        console.log(newReservation);
-        //initTable();
 
+        reservations.push(newReservation);
+        
+        initTable();
     }
 }
 
 const clickDate = (event) => {
-    // 예약정보 초기화
+    //예약정보 초기화 하자
     newReservation = {
-        "name" : undefined,
-        "room" : undefined,
-        "date" : undefined,
-        "time" : undefined,
-        "washingmachine" : undefined,
-        "notification" : true
+        "name":undefined,
+        "room":undefined,
+        "date":undefined,
+        "time":undefined,
+        "washingmachine":undefined,
+        "notification":true
     };
 
-    // 날짜 data 가져오기
+
+    //날짜 data 가져오자
     const dateString = event.target.dataset.date;
     const dateDate = new Date(dateString);
 
-    // 날짜 data 보관
-    newReservation.date = dateDate;
+    //날짜 data 보관하자
+    newReservation.date = dateDate
 
-    // 2페이지로 가기
+    //2페이지로 가자
     setPage(2);
 }
 
@@ -110,92 +115,113 @@ setPage(1);
 
 const initWashingmachineTime = () => {
     let allWashingmachineTime = {};
-    let washingmachines;    // 세탁기 번호 모음
+    let washingmachines;    //세탁기 번호 모음
 
-    // 초기 데이터 세팅하기 : {"1": ["1"],["2"],["3"], "2": ["1"],["2"],["3"], "3": ["1"],["2"],["3"]}
-    // allData.washingmachine에서 하나씩 꺼내기 => washingmachine
+    //초기 데이터 세팅하자 : {"1" : ["1", "2", "3"], "2" : ["1", "2", "3"], "3" : ["1", "2", "3"]}
+    //allData.washingmachine 에서 하나씩 꺼내자 => washingmachine
     allData.washingmachine.forEach((washingmachine) => {
-        allWashingmachineTime[washingmachine] = Object.keys(allData.time); // aWT["1"] = ["1", "2", "3"] => aWT = {"1","2","3"]}
+        allWashingmachineTime[washingmachine] = Object.keys(allData.time); //awt["1"] = ["1", "2", "3"] => awt = {"1" : ["1", "2", "3"]}
     });
 
-        // 선택한 날짜의 요일 구하기   
-        let weekday = newReservation.date.getDay();   
+    //선택한 날짜의 요일 구하자
+    let weekday = newReservation.date.getDay();
 
-        // 그 요일의 미리 예약된 세탁기와 시간 파악하기            
-        // 예약된 게 있으면 select 목록에서 빼기         
-        weeklyReservations.forEach((weeklyReservation) => {
-            if (weeklyReservation.weekday === weekday) {
-                const { washingmachine, time} = weeklyReservation;
-                // const washingmachine = weeklyReservation.washingmachine;
-                // const time = weeklyReservation.time;
-                const index = allWashingmachineTime[washingmachine].indexOf(String(time));      // 1 -> "1"
-                if (index > -1) {   // 예약된 시간 찾았다면
-                    allWashingmachineTime[washingmachine].splice(index, 1);     // 그 시간 빼기
-                }
+    //그 요일의 미리 예약된 세탁기와 시간 파악하자
+    //예약된게 있으면 select 목록에서 빼자
+    weeklyReservations.forEach((weeklyReservation) => {
+        if(weeklyReservation.weekday === weekday) {
+            const { washingmachine, time } = weeklyReservation;
+            // const washingmachine = getWeeklyReservation.washingmachine;
+            // const time = getWeeklyReservation;
+
+            const index = allWashingmachineTime[washingmachine].indexOf(String(time));  //1 -> "1"
+            if(index > -1) {    //예약된 시간 찾았다면
+                allWashingmachineTime[washingmachine].splice(index, 1); //그 시간 빼자
             }
-        });
-
-        
-        // TODO: 사용자가 예약한 내용도 위의 것을 다 파악해서 빼자 
-
-        
-
-        // select들(세탁기, 번호, 시간들) 만들기
-        washingmachineSelect.innerHTML = ""; //세탁기option 없애자
-        washingmachines = Object.keys(allWashingmachineTime);
-        //예약할 시간이 없으면, 세탁기도 번호도 빼자            allWashingmachineTime = {세탁기 번호:[시간,시간,시간]}
-        washingmachines = washingmachines.filter((washingmachine) => allWashingmachineTime[washingmachine].length > 0);
-
-        washingmachines.forEach((washingmachine) => {
-            // <option value="1">1번 세탁기</option>
-            // option 태그 만들기
-            const newOption = document.createElement("option");
-
-            // 값 넣기
-            newOption.value = washingmachine;
-
-            // 텍스트 넣기
-            newOption.textContent = `${washingmachine}번 세탁기`;
-
-            // washingmachineSelect에 저거 넣기
-            washingmachineSelect.appendChild(newOption);
-        });
-
-        const initTime = () => {
-            const selectWashingmachine = washingmachineSelect.value;    // 선택한 세탁기 option의 value
-
-            timeSelect.innerHTML = ""; //시간 option없애자
-            allWashingmachineTime[selectWashingmachine].forEach((time) => {
-                // <option value="1"> 7시 ~8시 10분 </option>
-                const newOption = document.createElement("option");
-                newOption.value = time;
-                newOption.textContent = allData.time[time];       // "2" -> 8시 10분 ~ 9시 20분, time -> allData.time[time]
-
-                timeSelect.appendChild(newOption);
-            });
         }
-        initTime();
+    });
 
-        //세탁기 번호가 바뀌면 다시 시간을 불러오자
-        // washingmachineSelect.onchange = initTime;
+    // TODO : 사용자가 예약한 내용도 위의 것을 다 파악해서 빼자
+    
+    //select 들 : (세탁기 번호, 시간들) 만들자
+    washingmachineSelect.innerHTML = "";    // 세탁기 option 없애자
 
-        // 3page에 세탁기, 시간 넘기자
+    washingmachines = Object.keys(allWashingmachineTime);
 
+    // 예약 할 시간이 없으면, 세탁기도 번호도 빼기
+    washingmachines = washingmachines.filter((washingmachine) => allWashingmachineTime[washingmachine].length > 0);
+
+    washingmachines.forEach((washingmachine) => {
+        // <option value="1">1번세탁기</option>
+        // option 태그 만들자
+        const newOption = document.createElement("option");
+
+        // 값 넣자
+        newOption.value = washingmachine;
+
+        // 텍스트 넣자
+        newOption.textContent = `${washingmachine}번 세탁기`;
+
+        // washingmachineSelect에 넣자
+        washingmachineSelect.appendChild(newOption);
+    });
+
+    const initTime = () => {
+        const selectedWashingmachine = washingmachineSelect.value; //선택한 세탁기 option의 value
+        timeSelect.innerHTML = "";  // 시간 option 없애기
+        allWashingmachineTime[selectedWashingmachine].forEach((time) => {
+            //<option value = "1">7시 ~ 8시 10분</option>
+
+            const newOption = document.createElement("option");
+            newOption.value = time;
+            newOption.textContent = allData.time[time]; //"2" -> allData.time["2"](8시 10분 ~ 9시 20분).time -> allData.time[time]
+
+            timeSelect.appendChild(newOption);
+        });
+    }   
+    initTime();
+
+    // 세탁기 번호가 바뀌면, 다시 시간을 불러오기
+    washingmachineSelect.onchange = initTime;
+
+    //3page에 세탁기, 시간 넘기자
 
 }
-const initRoomName= () => {
-//     모든 호실 표시하자	
-    //allData에서 방 정보 가져와서 <option value="401">401호</option> 만들어서roomSelect에 자식으로 붙이자
+
+const initRoomName = () => {
+    // 모든 호실 표시하기      
+    // allData에서 방 정보 가져와서 <option value="401">401호</option> 만들어서 roomSelect의 자식으로 붙이기
     let rooms = allData.room;
     let optionString = "";
     rooms.forEach((room) => {
-    optionString += `<option value="${room}">${room}호</option>`;
+        optionString += `<option value="${room}">${room}호</option>`;
     });
     roomSelect.innerHTML = optionString;
-    console.log(optionString);
-//     이름 초기화 하자	
-        nameInput.value = "";
-//     4page에 호실,이름 넘기자		
-  
 
+    // 이름 초기화 하기      
+    nameInput.value = "";
+
+    // 4page에 호실, 이름 넘기기
+
+}
+
+const initTable = () => {
+    // 사용자가 예약한 내용들(reservations) 보여주기
+    // .board-container 내용 뒤에, <div class"item">내용들</div>
+    let itemString = boardContainerDivInitString;   // 제목만 있는 스트링
+    reservations.forEach((reservation) => {
+        const year = reservation.date.getFullYear();
+        const month = reservation.date.getMonth() + 1;
+        const date = reservation.date.getDate();
+
+        itemString += `
+        <div class="item">${reservation.name}</div>
+        <div class="item">${reservation.room}호</div>
+        <div class="item">${year}년 ${month}월 ${date}일</div>
+        <div class="item">${allData["time"][reservation.time]}</div>
+        <div class="item">${reservation.washingmachine}번 세탁기</div>
+        <div class="item">${reservation.notification?"🔔":"🔔❌"}</div>
+        `;
+    });
+    boardContainerDiv.innerHTML = itemString;   // string -> 표에 표시하기
 }
